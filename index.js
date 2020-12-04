f6a6a11d807a616c08275439a3feb39cdf75dfc7
Andrew Simmons <neon.duncan@gmail.com>
2020-12-04 22:05:14 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,21 @@
+var recipes = {cake:"eggs", bread: "flour"}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  var newObject = Object.assign({}, object, {key: value})
+  return newObject
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  
+}
+
+function deleteFromObjectByKey(object, key) {
+  
+  
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  
+}
+
+console.log(updateObjectWithKeyAndValue(recipes, soup, "water"))
