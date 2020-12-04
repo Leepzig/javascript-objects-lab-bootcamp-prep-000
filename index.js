973dfd4e139c144242fef3085eed97d7e9ba487b
Andrew Simmons <neon.duncan@gmail.com>
2020-12-04 23:09:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {cake:"eggs", bread: "flour"}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var newObject = Object.assign({}, object, {key: value})
+  var newObject = Object.assign({}, object, {[key]: value})
   return newObject
 }
 
